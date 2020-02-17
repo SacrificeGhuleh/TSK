fd02e8a32b38197424a1821dc3aa7ad5d882c21d
Richard Zvonek <zvonek.r@seznam.cz>
2020-02-17 10:21:53 +0100

    TSK cv01

---
+++ src/main/java/tsk2020/App.java
@@ -0,0 +1,34 @@
+package tsk2020;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static boolean isTriangle(double a, double b, double c) {
+        if (a <= 0) return false;
+        if (b <= 0) return false;
+        if (c <= 0) return false;
+
+        //a is max
+        if (a >= b && a >= c) {
+            return b + c > a;
+        }
+
+        //b is max
+        if (b >= a && b >= c) {
+            return a + c > b;
+        }
+
+        //c is max
+        if (c >= a && c >= b) {
+            return a + b > c;
+        }
+        return false;
+    }
+
+
+}
---
+++ src/test/java/tsk2020/AppTest.java
@@ -0,0 +1,42 @@
+package tsk2020;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @ParameterizedTest(name = "Test trojuhelniku {index}, strany a = {0}, b = {1}, c = {2}, Ocekavany vystup = {3}")
+    @MethodSource("edgeProvider")
+    public void testParametrizeTriangle(double a, double b, double c, boolean result) {
+        assertEquals(result, App.isTriangle(a, b, c));
+    }
+
+    static Stream<Arguments> edgeProvider() {
+        return Stream.of(
+                Arguments.arguments(10, 10, 10, true),
+                Arguments.arguments(10, 20, 15, true),
+                Arguments.arguments(30, 20, 15, true),
+                Arguments.arguments(20, 15, 30, true),
+                Arguments.arguments(15, 30, 25, true),
+                Arguments.arguments(1.5, 1, 1, true),
+                Arguments.arguments(2.1, 3.1, 4.1, true),
+                Arguments.arguments(0, 20, 15, false),
+                Arguments.arguments(10, 0, 15, false),
+                Arguments.arguments(10, 20, 0, false),
+                Arguments.arguments(-10, 20, 15, false),
+                Arguments.arguments(10, -20, 15, false),
+                Arguments.arguments(10, 20, -15, false)
+        );
+    }
+}
+
+
